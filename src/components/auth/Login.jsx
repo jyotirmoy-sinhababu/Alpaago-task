ce38e4dc882fede907a5d9d633f0868b736420b9
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-05 02:19:06 +0530

    style added to login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <form>
-      <input type='email' placeholder='User Email' className='border-2' />
-      <input type='password' placeholder='User Password' className='border-2' />
-      <button>Log In</button>
+    <form className='flex flex-col gap-8'>
+      <input
+        type='email'
+        placeholder='User Email'
+        className='border-2 h-[55px] px-[10px] w-[520px]'
+      />
+      <input
+        type='password'
+        placeholder='User Password'
+        className='border-2 h-[55px] px-[10px] w-[520px]'
+      />
+      <button className='w-[212px] border-2 h-[55px] flex justify-center items-center'>
+        Log In
+      </button>
     </form>
   );
 };
