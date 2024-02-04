5e7281f133a1c92b82fb24c9d6f3692fe1b7c8ae
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-05 02:32:50 +0530

    login functionality added

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import { useState } from 'react';
+
+import useLogin from '../../hooks/useLogin';
 
 const Login = () => {
+  const { loading, error, loginUser } = useLogin();
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
   return (
-    <form className='flex flex-col gap-8'>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        loginUser(inputs);
+      }}
+      className='flex flex-col gap-8'
+    >
       <input
         type='email'
         placeholder='User Email'
         className='border-2 h-[55px] px-[10px] w-[520px]'
+        onChange={(e) => {
+          setInputs({ ...inputs, email: e.target.value });
+        }}
       />
       <input
         type='password'
         placeholder='User Password'
         className='border-2 h-[55px] px-[10px] w-[520px]'
+        onChange={(e) => {
+          setInputs({ ...inputs, password: e.target.value });
+        }}
       />
-      <button className='w-[212px] border-2 h-[55px] flex justify-center items-center'>
+      <button
+        type='submit'
+        className='w-[212px] border-2 h-[55px] flex justify-center items-center'
+      >
         Log In
       </button>
     </form>
---
+++ src/hooks/useLogin.js
@@ -0,0 +1,31 @@
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth, firestore } from '../firebase/Firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
+const useLogin = () => {
+  const [signInWithEmailAndPassword, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const loginUser = async (inputs) => {
+    if (!inputs.email || !inputs.password) {
+      console.log('error');
+    }
+    try {
+      const userCred = await signInWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      );
+      if (userCred) {
+        const docRef = doc(firestore, 'users', userCred.user.uid);
+        const docSnap = await getDoc(docRef);
+        localStorage.setItem('user-info', JSON.stringify(docSnap.data()));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { loading, error, loginUser };
+};
+
+export default useLogin;
