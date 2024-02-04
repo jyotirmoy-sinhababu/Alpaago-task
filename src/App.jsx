b8ef61b657e0455cf5d5f6bf812c84e28f74635a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-04 13:34:18 +0530

    homepage created and auth comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import HomePage from './page/homePage/HomePage';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
