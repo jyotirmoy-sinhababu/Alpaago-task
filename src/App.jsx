8f0406fae26a9eaddcc67b441418004edd3a6d82
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-04 17:15:30 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './page/homePage/HomePage';
+import AuthPage from './page/authPage/AuthPage';
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='auth' element={<AuthPage />} />
+      </Routes>
     </>
   );
 }
