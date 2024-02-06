fd05dcdc4ae4891de885a5e390b7f5df41564e68
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-07 00:11:54 +0530

    updated

--- src/components/tableComp/TableComp.jsx
+++ src/components/tableComp/TableComp.jsx
@@ -11,7 +11,7 @@ const TableComp = () => {
   return (
     <>
       {' '}
-      <table className='w-[710px] text-xl text-left rtl:text-right text-gray-600 dark:text-gray-400'>
+      <table className='w-[710px] text-xl text-left rtl:text-right text-gray-600 dark:text-gray-400 scroll-m-2'>
         <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
           <tr>
             <th scope='col' className='px-6 py-3'>
--- src/components/tableComp/TableRowComp.jsx
+++ src/components/tableComp/TableRowComp.jsx
@@ -33,10 +33,6 @@ const TableRowComp = ({ user }) => {
     }
   };
 
-  const refreshPage = () => {
-    window.location.reload(false);
-  };
-
   return (
     <tr
       className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'
@@ -117,7 +113,6 @@ const TableRowComp = ({ user }) => {
               className='block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-2xl px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
               onClick={() => {
                 handleDelete();
-                refreshPage();
               }}
             >
               <MdDelete />
